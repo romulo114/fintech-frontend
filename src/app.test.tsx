52fdaec4be25e78c179b3e0618dd7c1b1a6b32bb
romulo114 <romulo.dev114@gmail.com>
2021-09-10 21:04:59 +0300

    test fixed

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,6 +4,6 @@ import { App } from './app';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = screen.getByText(/Home Page/i);
+  expect(div).toBeInTheDocument();
 });
