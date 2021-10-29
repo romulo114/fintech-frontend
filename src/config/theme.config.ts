3f9663d1736846f966aae55259bbd063816db6ed
romulo114 <romulo.dev114@gmail.com>
2021-10-29 20:30:47 +0300

    missing files

---
+++ src/config/theme.config.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material/styles"
+
+export const theme = createTheme({
+  // palette: {
+
+  // },
+  // components: {
+
+  // },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    }
+  }
+})
