4208a62db32642fe733693bdd677d0334d0b2b70
romulo114 <romulo.dev114@gmail.com>
2021-11-10 06:06:00 +0300

    support page done

--- src/containers/pages/home/support.tsx
+++ src/containers/pages/home/support.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container } from '@mui/material'
 
 export const SupportPage: React.FC = () => {
   return (
-    <div>
-      support page
-    </div>
+    <Container maxWidth='lg'>
+      <h1>
+        Fithm
+      </h1>
+      Contact Us: 
+      <a href='mailto:info@fithm.com' className='contact-link'>
+        info@fithm.com
+      </a>
+    </Container>
   )
 }
