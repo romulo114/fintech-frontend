fb2a2f1f87bbceed1c57bbe5d2fb0b50bd36c17e
romulo114 <romulo.dev114@gmail.com>
2021-11-13 03:56:31 +0300

    test error

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,6 +4,6 @@ import { App } from './app'
 
 test('renders learn react link', () => {
   render(<App />)
-  const anchor = screen.getByText(/Try fithm now/i)
+  const anchor = screen.getByText(/Invest Like A Professional/i)
   expect(anchor).toBeInTheDocument()
 })
