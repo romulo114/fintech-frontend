b17fade2c0129dcc03c325aa6f3b3886f2cdca61
romulo114 <romulo.dev114@gmail.com>
2022-09-13 17:53:26 +0800

    feat: new components for global notification and dialog

---
+++ src/components/shared/app-dialog.tsx
@@ -0,0 +1,33 @@
+import { Modal, Typography, Box } from '@mui/material'
+import { useDialog } from 'hooks/use-dialog';
+
+export const AppNotification = () => {
+	const { open, title, body, onClose } = useDialog();
+
+	return (
+		<Modal
+			open={open}
+			onClose={onClose}
+			aria-labelledby='dialog-header'
+			aria-describedby='dialog-body'
+		>
+			<Box className='dialog'>
+				<section className='header'>
+					{typeof title === 'string' ? (
+						<Typography id='dialog-header' variant='h6' component='h2'>
+							{title}
+						</Typography>
+					) : title}
+				</section>
+
+				<section className='body'>
+					{typeof body === 'string' ? (
+						<Typography id='dialog-body'>
+							{body}
+						</Typography>
+					) : body}
+				</section>
+			</Box>
+		</Modal>
+	)
+};
